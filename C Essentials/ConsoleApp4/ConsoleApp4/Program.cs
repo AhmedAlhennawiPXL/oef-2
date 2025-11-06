06849f239bb35da708edde0b125623cbec86e14a
Michael Shen <michael.shen@student.pxl.be>
2025-11-06 10:16:54 +0100

    tweede nigga

--- C Essentials/ConsoleApp4/ConsoleApp4/Program.cs
+++ C Essentials/ConsoleApp4/ConsoleApp4/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, nigga!");
+
+            Console.Write("what is your name my boi!");
+            string Naam = Console.ReadLine();
+
+            Console.WriteLine($"welcome {Naam}!");
         }
     }
 }
